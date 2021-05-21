0bcb27996f4731f2ed97ffddeaba6ecfaf1d069f
FranzFialho <franzfialho@gmail.com>
2021-05-20 22:44:14 -0300

    Adicionar arquivos de projeto.

---
+++ Database/MinhasTarefasContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinhasTarefasAPI.Models;
+
+namespace MinhasTarefasAPI.Context
+{
+    public class MinhasTarefasContext : DbContext
+    {
+        public MinhasTarefasContext(DbContextOptions<MinhasTarefasContext> options) 
+            :base(options)
+        {
+           
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+    }
+}
---
+++ Models/UsuarioDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasAPI.Models
+{
+    public class UsuarioDTO
+    {
+        [Required(ErrorMessage ="Nome Obrigatório")]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="Email Inválido")]
+        public string Email { get; set; }
+
+        [Required]  
+        public string Senha { get; set; }
+
+        [Required]
+        [Compare("Senha")]
+        public string ConfirmacaoSenha  { get; set; }
+
+    }
+}
